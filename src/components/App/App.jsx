a057867fedf780d4d9cd7db20a0092ccb3b63468
HelenYazh <olenka.yazh05@gmail.com>
2024-07-31 02:25:46 +0300

    add FriendList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Profile from '../Profile/Profile';
 import userData from "../../userData.json";
+import FriendList from '../FriendList/FriendList';
+import friends from '../../friends.json'
 
 const App = () => {
  return (
@@ -11,7 +13,8 @@ const App = () => {
     location={userData.location}
     image={userData.avatar}
     stats={userData.stats}
-   />
+     />
+     <FriendList friends={friends}></FriendList>
   </>
  );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,24 @@
+import css from "./FriendList.module.css";
+import clsx from "clsx";
+
+const FriendList = ({ friends }) => {
+    return (<ul className={css.list}>
+        {friends.map((friend) =>( 
+            <li className={css.card} key={friend.id}>
+                <FriendListItem avatar={friend.avatar} name={friend.name} isOnline={friend.isOnline}/>
+            </li>
+        ))}
+    </ul>);
+}
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+    return (
+        <div>
+            <img src={avatar} alt="Avatar" width="48" />
+            <p>{name}</p>
+            <p className={css[clsx(isOnline ? "online" : "offline")]}>{isOnline ? "Online" : "Offline"}</p>
+        </div>
+    );
+}
+
+export default FriendList;
